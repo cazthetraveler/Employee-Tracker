4d9298acd578137e0425f4f4cf25c4b7a4f0f83e
MultiMouse <olmos.luisd@outlook.com>
2023-11-10 19:23:24 -0600

    added init function and started inquirer prompt

--- index.js
+++ index.js
@@ -10,3 +10,35 @@ const db = mysql.createConnection(
     },
     console.log(`Connected to the employees_db database!`)
 );
+
+function init() {
+    inquirer.prompt([
+        {
+            type: "list",
+            message: "What would you like to do?",
+            choices: ["View All Departments", "View All Roles", "View All Employees", "Add a Department", "Add a Role", "Add an Employee", "Update an Employee Role", "QUIT"],
+            name: "action"
+        }
+    ]).then((data) => {
+        const action = data.action;
+        switch(action) {
+            case "View All Departments":
+                db.query(`SELECT * FROM department`, function(err, results) {
+                    if (err) {
+                        console.error(err);
+                    } else {
+                        console.table(results);
+                        console.log(`Viewing all departments!`);
+                        init();
+                    };
+                })
+                break;
+            case "QUIT":
+                console.log(`Hasta la pizza!`);
+                db.end();
+                break;
+        };
+    });
+};
+
+init();
